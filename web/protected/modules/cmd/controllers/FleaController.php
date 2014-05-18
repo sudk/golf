ee9e102252425094580a14f7eb229f7e5144bab2
DunKuai Su <sudunkuai@gmail.com>
2014-05-18 13:17:48 +0800

    123
    123

--- web/protected/modules/cmd/controllers/FleaController.php
+++ web/protected/modules/cmd/controllers/FleaController.php
@@ -165,15 +165,15 @@ class FleaController extends CMDBaseController
             $command->bindParam(":record_time",$record_time, PDO::PARAM_STR);
             $command->bindParam(":user_id", $user_id, PDO::PARAM_STR);
             $command->execute();
-            if(!isset(Yii::app()->command->cmdObj->imgs)&&count(Yii::app()->command->cmdObj->imgs)){
+            if(isset(Yii::app()->command->cmdObj->imgs)&&count(Yii::app()->command->cmdObj->imgs)){
                 $type=Img::TYPE_FLEA;
                 foreach(Yii::app()->command->cmdObj->imgs as $img){
-                    $img=str_replace(Img::IMG_PATH,'',$img);
+                    $img=Img::GetBasePath($img);
                     $sql="insert into g_img (relation_id,`type`,`img_url`,record_time) values (:relation_id,:type,:img_url,:record_time)";
                     $command = $conn->createCommand($sql);
                     $command->bindParam(":relation_id",$id, PDO::PARAM_STR);
                     $command->bindParam(":type",$type, PDO::PARAM_STR);
-                    $command->bindParam(":img_url",Img::GetBasePath($img), PDO::PARAM_STR);
+                    $command->bindParam(":img_url",$img, PDO::PARAM_STR);
                     $command->bindParam(":record_time",$record_time, PDO::PARAM_STR);
                     $command->execute();
                 }
@@ -263,15 +263,15 @@ class FleaController extends CMDBaseController
             $command->bindParam(":type",$type, PDO::PARAM_STR);
             $command->execute();
 
-            if(!isset(Yii::app()->command->cmdObj->imgs)&&count(Yii::app()->command->cmdObj->imgs)){
+            if(isset(Yii::app()->command->cmdObj->imgs)&&count(Yii::app()->command->cmdObj->imgs)){
 
                 foreach(Yii::app()->command->cmdObj->imgs as $img){
-                    $img=str_replace(Img::IMG_PATH,'',$img);
+                    $img=Img::GetBasePath($img);
                     $sql="insert into g_img (relation_id,`type`,`img_url`,record_time) values (:relation_id,:type,:img_url,:record_time)";
                     $command = $conn->createCommand($sql);
                     $command->bindParam(":relation_id",Yii::app()->command->cmdObj->id, PDO::PARAM_STR);
                     $command->bindParam(":type",$type, PDO::PARAM_STR);
-                    $command->bindParam(":img_url",Img::GetBasePath($img), PDO::PARAM_STR);
+                    $command->bindParam(":img_url",$img, PDO::PARAM_STR);
                     $command->bindParam(":record_time",$record_time, PDO::PARAM_STR);
                     $command->execute();
                 }
