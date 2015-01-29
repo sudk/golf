700cfb2527ffd71495905dc87fdd700a25298348
DunKuai Su <sudunkuai@gmail.com>
2015-01-29 23:28:15 +0800

    123
    123

--- web/protected/commands/ToolsCommand.php
+++ web/protected/commands/ToolsCommand.php
@@ -27,4 +27,19 @@ class ToolsCommand extends CConsoleCommand
         MchtVisit::VisitCompute();
         echo "end compute";
     }
+
+    public function actionCheckPayTime(){
+        date_default_timezone_set('Asia/Shanghai');
+        $now=date("Y-m-d H:i:s",strtotime("-240 seconds"));
+        $rows=Yii::app()->db->createCommand()
+            ->select("order_id")
+            ->from("g_order")
+            ->where("last_pay_time <= '{$now}' and status=1")
+            ->queryAll();
+        if(count($rows)){
+            foreach($rows as $row){
+                Order::Cancel($row['order_id']);
+            }
+        }
+    }
 }
